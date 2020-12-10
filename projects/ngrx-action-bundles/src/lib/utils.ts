002e4179054ba9ed80cacdc2f077c687659ca102
Ilia Idakiev <ilia.idakiev@gmail.com>
2020-12-10 11:40:19 +0200

    updated to ts 4.1.2

--- projects/ngrx-action-bundles/src/lib/utils.ts
+++ projects/ngrx-action-bundles/src/lib/utils.ts
@@ -19,3 +19,8 @@ export function deepAssign<R extends any[]>(...sources: R): UnionToIntersection<
   }
   return target as UnionToIntersection<ElementOf<R>>;
 }
+
+function capitalize<T extends string>(s: T): Capitalize<T> {
+  if (typeof s !== 'string') { return '' as Capitalize<T>; }
+  return s.charAt(0).toUpperCase() + s.slice(1) as Capitalize<T>;
+}
