f27114bd086730dbe6e12fb35c1edbe58dfd368c
Ilia Idakiev <ilia.idakiev@gmail.com>
2020-12-15 15:21:57 +0200

    removed deleted stuff

--- projects/ngrx-action-bundles/src/public-api.ts
+++ projects/ngrx-action-bundles/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngrx-action-bundles
  */
 export * from './lib/ngrx-action-bundles.service';
-export * from './lib/ngrx-action-bundles.component';
 export * from './lib/ngrx-action-bundles.module';
 
 export * from './lib/lib';
